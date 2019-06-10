708f45b2fc728dd540562f33ada956843ef69cee
josayarh <johakim.sayarh@gmail.com>
2019-06-10 13:01:11 +0100

    forgot some stuff

---
+++ Ai/BaseAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/***
+ * Interface used to define 
+ */
+public interface BaseAI
+{
+    /***
+     * Performs a transition from attack state to wander state
+     */
+    void attackWanderTransition();
+    /***
+     * Performs a transition from wander state to attack state
+     */
+    void wanderAttackTransistion(GameObject target);
+}
---
+++ Ai/NPCDetector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NpcType
+{
+    PlayerBot,
+    Enemy
+}
+
+public class NPCDetector
+{
+    private Dictionary<NpcType, List<GameObject>> NpcDictionary = new Dictionary<NpcType, List<GameObject>>();
+    
+    public NPCDetector()
+    {
+        Pool pool = Pool.Instance;
+        
+        NpcDictionary.Add(NpcType.Enemy, pool.EnemyList);
+        NpcDictionary.Add(NpcType.PlayerBot, pool.PlayerBotList);
+    }
+
+    public GameObject getNpcInRange(NpcType npcType, Vector3 position, float range)
+    {
+        List<GameObject> npcList;
+        if (NpcDictionary.TryGetValue(npcType, out npcList))
+        {
+            foreach (GameObject npc in npcList)
+            {
+                if (Vector3.Distance(npc.transform.position, position) < range)
+                    return npc;
+            }
+        }
+
+        return null;
+    }
+}
